4b2e65b8a460c224c31c672fb2c8795014e6bb29
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-11-29 00:51:02 +0100

    update

--- js/testapi.js
+++ js/testapi.js
@@ -1,11 +1,67 @@
-export const BASE_URL = "https://nf-api.onrender.com";
+const card = document.querySelector(".row"); // card
 
-export const registerURL = "/api/v1/social/auth/register";
+const image = document.createElement("img"); // image
+image.classList.add("d-flex", "img-thumbnail", "p-2", "mb-2", "mx-auto");
 
-export const LoginAuth = "/api/v1/social/auth/login";
+const newCard = document.createElement("div"); // card-body
+newCard.className = "card-body";
 
-export const POSTregisterURL = `${BASE_URL}${registerURL}`; // for register form url
+const NameOfTittle = document.createElement("h5");
+NameOfTittle.className = "card-title";
+const subtitle = document.createElement("h6");
+subtitle.classList.add("card-subtitle", "mb-2", "text-muted");
+const paraGraph_text = document.createElement("p");
+paraGraph_text.className = "card-text";
+const img = document.createElement("img");
+img.classList.add("d-flex", "img-thumbnail", "p-2", "mb-2 ");
+const span = document.createElement("span");
+span.className = "numbers";
+const button = document.createElement("button");
+button.className = "cta_btn";
 
-export const LoginAuthUser = `${BASE_URL}${LoginAuth}`; // login form url
+function add() {
+  card.appendChild(image);
+  image.appendChild(newCard);
+  newCard.appendChild(NameOfTittle);
+  newCard.appendChild(subtitle);
+  newCard.appendChild(paraGraph_text);
+  newCard.appendChild(img);
+  newCard.appendChild(span);
+  newCard.appendChild(button);
+}
+add();
+console.log(add());
+// const row = document.querySelector("div.row");
+// console.log(row);
 
-console.log("object");
+// const mainDiv = document.createElement("div");
+// console.log(mainDiv);
+// const img = document.createElement("img");
+// console.log(img);
+// const div = document.createElement("div");
+// console.log(div);
+// div.className = "card-body";
+
+// const title = document.createElement("h5");
+// console.log(title);
+
+// title.innerText = "hello";
+
+// const title_text = document.createElement("h6");
+// console.log(title_text);
+
+// const pText = document.createElement("p");
+
+// console.log(pText);
+
+// const img_1 = document.createElement("img");
+// console.log(img_1);
+
+// mainDiv.classList.add("col-12", "col-lg-5", "col-md-12", "col-sm-12");
+// div.className = "card";
+// img.setAttribute("src", "postImg");
+
+// title.className = "card-title";
+// title_text.classList.add("card-subtitle", "mb-2", "text-muted");
+// pText.className = "card-text";
+// img_1.setAttribute("src", "post_img_img");
