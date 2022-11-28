78b71a0ead2b1861d5c8b0b17e9908447df0225f
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-11-28 23:06:49 +0100

    update

--- js/find.js
+++ js/find.js
@@ -109,21 +109,21 @@ myModule.publicMethod();
 
 ////------------------
 
-// import { registerUser } from "../handler/register.mjs";
-// import { POSTregisterURL } from "../api/api.mjs";
-// registerUser(POSTregisterURL);
-// const form = document.querySelector("#registerForm");
-// console.log(form);
+import { registerUser } from "../handler/register.mjs";
+import { POSTregisterURL } from "../api/api.mjs";
+registerUser(POSTregisterURL);
+const form = document.querySelector("#registerForm");
+console.log(form);
 
-// //
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const form = e.target;
-//   const name = form.name.value;
-//   const email = form.email.value;
-//   const password = form.password.value;
+//
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const form = e.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const password = form.password.value;
 
-//   const registerForm = { form, name, email, password };
-//   console.log(registerForm);
-//   // send it to api
-// });
+  const registerForm = { form, name, email, password };
+  console.log(registerForm);
+  // send it to api
+});
